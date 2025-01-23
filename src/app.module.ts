96ea5fe3a075ab639e641db1ca32fca8db8054a1
Faisal Ismail <faisalkhan7842540@gmail.com>
2025-01-23 19:49:33 +0500

    - migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,27 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { Artist } from './artists/artist.entity';
-import { User } from './users/user.entity';
-import { Song } from './songs/song.entity';
-import { Playlist } from './playlists/playlist.entity';
 import { PlayListModule } from './playlists/playlists.module';
 import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ArtistsModule } from './artists/artists.module';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      database: 'spotify-clone',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      entities: [Song, Artist, User, Playlist],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     SongsModule,
     PlayListModule,
     UserModule,
